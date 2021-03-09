39e81ffa918c702f5026290045317569d64fa8fd
Matt Howard <matthoward366@users.noreply.github.com>
2021-03-09 13:00:39 -0600

    #70 Update schema references to use https (#71)

--- common-pipeline/pipeline-flow/upgrade/upgrade-flow.js
+++ common-pipeline/pipeline-flow/upgrade/upgrade-flow.js
@@ -84,7 +84,7 @@ function _update0to1(pipelineFlow) {
  */
 function _update1to2(pipelineFlow) {
 	pipelineFlow.version = "2.0";
-	pipelineFlow.json_schema = "http://api.dataplatform.ibm.com/schemas/common-pipeline/pipeline-flow/pipeline-flow-v2-schema.json";
+	pipelineFlow.json_schema = "https://api.dataplatform.ibm.com/schemas/common-pipeline/pipeline-flow/pipeline-flow-v2-schema.json";
 
 	// --> Changed the binding node. Instead of just a single input or output port,
 	// binding nodes can have either an array of input ports or an array of output ports.
@@ -133,7 +133,7 @@ function _update1to2(pipelineFlow) {
  */
 function _update2to3(pipelineFlow) {
 	pipelineFlow.version = "3.0";
-	pipelineFlow.json_schema = "http://api.dataplatform.ibm.com/schemas/common-pipeline/pipeline-flow/pipeline-flow-v3-schema.json";
+	pipelineFlow.json_schema = "https://api.dataplatform.ibm.com/schemas/common-pipeline/pipeline-flow/pipeline-flow-v3-schema.json";
 
 	return pipelineFlow;
 }
